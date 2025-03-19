e744ffecfb8e35b4b6e1bd59a6c5bc6cda7b7103
DinithaSasinduDissanayake <dinithasasindu10@gmail.com>
2025-03-19 11:16:06 +0530

    Updated the readme with the folder structure

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 // Connect to MongoDB
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/smartbin';
+const MONGODB_URI = process.env.MONGODB_URI ;
 mongoose
   .connect(MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
